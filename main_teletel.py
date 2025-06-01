52202fe60f93ffd958cbcb78985eb2eb12e1012e
Pcbuff <pc.buffard@qwest.fr>
2025-06-01 23:13:26 +0200

    Update main_teletel.py

--- main_teletel.py
+++ main_teletel.py
@@ -70,20 +70,20 @@ def page_accueil():
     m.inverse(False)
 
     m.pos(20, 31)
-    m._print("└───────────────")
+    m._print("")
     m.pos(21, 32)
     m.inverse()
     m._print("Guide")
     m.inverse(False)
 
     m.pos(22, 31)
-    m._print("───────────────┘")
+    m._print("")
     m.pos(23, 32)
     m.color(m.vert)
     m._print("Sommaire")
 
     m.pos(24, 31)
-    m._print("└───────────────")
+    m._print("")
     m.pos(24, 32)
     m.color(m.blanc)
     m._print("Cx/Fin")
