c53fbdc53428b1057266114e61ea20728cb12dcd
Pcbuff <pc.buffard@qwest.fr>
2025-06-01 23:45:44 +0200

    Update main_teletel.py

--- main_teletel.py
+++ main_teletel.py
@@ -48,7 +48,7 @@ def page_accueil():
 
     # Mentions légales
     m.pos(11, 10)
-    m._print("dont F. TELECOM 0,12 à la connexion")
+    m._print("dont 0,12F à la connexion")
     m.pos(12, 10)
     m._print("vers les DOM, ajouter 0,33F/min")
     m.pos(13, 10)
